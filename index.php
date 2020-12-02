783dbb761235f7e6e99f98d795ac7577373ed622
alexr61 <alexzanderrodriguez61@gmail.com>
2020-12-01 21:41:41 -0500

    First project try 1
    1st freelance design website post

---
+++ index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt"
+        crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed|Roboto+Slab" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>Klexed Freelance Graphic Designer</title>
+</head>
+
+<body>
+    <div id="slideout-menu">
+        <ul>
+            <li>
+                <a href="index.html">Home</a>
+            </li>
+            <li>
+                <a href="blogslist.html">Reviews</a>
+            </li>
+            <li>
+                <a href="blogpost.html">Portfolio</a>
+            </li>
+            <li>
+                <a href="about.html">About</a>
+            </li>
+            <li>
+                <a href="blog.html">Blog</a>
+            </li>
+            <li>
+                <a href="contact.html">Contact</a>
+            </li>
+            <li>
+                <input type="text" placeholder="Search Here">
+            </li>
+        </ul>
+    </div>
+
+    <nav>
+        <div id="logo-img">
+            <a href="#">
+               Alexander "Klexed" Rodriguez
+            </a>
+        </div>
+        <div id="menu-icon">
+            <i class="fas fa-bars"></i>
+        </div>
+        <ul>
+            <li>
+                <a class="active" href="index.html">Home</a>
+            </li>
+            <li>
+                <a href="blogslist.html">Reviews</a>
+            </li>
+            <li>
+                <a href="blogpost.html">Portfolio</a>
+            </li>
+            <li>
+                <a href="about.html">About</a>
+            </li>
+            <li>
+                <a href="blog.html">Blog</a>
+            </li>
+            <li>
+                <a href="contact.html">Contact</a>
+            </li>
+            <li>
+                <div id="search-icon">
+                    <i class="fas fa-search"></i>
+                </div>
+            </li>
+        </ul>
+    </nav>
+
+    <div id="searchbox">
+        <input type="text" placeholder="Search Here">
+    </div>
+
+    <div id="banner">
+        <h1>Klexed</h1>
+        <h3>Freelance Graphic Designer</h3>
+    </div>
+
+    <main>
+        <a href="blogslist.html">
+            <h2 class="section-heading">Latest Reviews</h2>
+        </a>
+
+        <section>
+            <div class="card">
+                <div class="card-image">
+                    <a href="blogslist.html">
+                        <img src="img/review 1.png" alt="Card Image">
+                    </a>
+                </div>
+
+                <div class="card-description">
+                    <a href="blogslist.html">
+                        <h3>Social media revamp for producer / artist Jonah Zed</h3>
+                    </a>
+                    <p>
+                        From the creation of a complete social media package for a music producer through the freelance website known as Fiverr.
+                    </p>
+                    <a href="blogslist.html" class="btn-readmore">See more</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-image">
+                    <a href="blogslist.html">
+                        <img src="img/review 3.png" alt="Card Image">
+                    </a>
+                </div>
+
+                <div class="card-description">
+                    <a href="blogslist.html">
+                        <h3>Twitter social media package for customer Snapchat filter company</h3>
+                    </a>
+                    <p>
+                    This creation is from a order that I completed that was exactly what "My Customer SnapFilters" was looking for a graphics package on Twitter.
+                    </p>
+                    <a href="blogslist.html" class="btn-readmore">See more</a>
+                </div>
+            </div>
+        </section>
+
+        <a href="blogpost.html">
+        	<h2 class="section-heading">Latest Designs</h2>
+    	</a>
+
+        <section>
+            <div class="card">
+                <div class="card-image">
+                    <a href="blogpost.html">
+                        <img src="img/3.jpg" alt="Card Image">
+                    </a>
+                </div>
+
+                <div class="card-description">
+                    <a href="blogpost.html">
+                        <h3>Youtuber Roddy Pammyz banner</h3>
+                    </a>
+                    <p>
+                        Simple yet effective YouTube banner for gaming commentator Roddy Pammyz.
+                    </p>
+                    <a href="blogpost.html" class="btn-readmore">See more</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-image">
+                    <a href="blogpost.html">
+                        <img src="img/2.jpg" alt="Card Image">
+                    </a>
+                </div>
+
+                <div class="card-description">
+                    <a href="blogpost.html">
+                        <h3>Minecraft Youtuber Survival YouTube banner</h3>
+                    </a>
+                    <p>
+                        A banner with the Minecraft font turned into a 3D text created in a software called Cinema 4D.
+                    </p>
+                    <a href="blogpost.html" class="btn-readmore">See more</a>
+                </div>
+            </div>
+        </section>
+
+        <footer>
+            <div id="left-footer">
+                <h3>Quick Links</h3>
+                <p>
+                    <ul>
+                        <li>
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li>
+                            <a href="about.html">About</a>
+                        </li>
+                        <li>
+                            <a href="privacy.html">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="blogslist.html">Reviews</a>
+                        </li>
+                        <li>
+                            <a href="blogpost.html">Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="blog.html">Blog</a>
+                        </li>
+                        <li>
+                            <a href="contact.html">Contact</a>
+                        </li>
+                    </ul>
+                </p>
+            </div>
+
+            <div id="right-footer">
+                <h3>Follow me on</h3>
+                <div id="social-media-footer">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <i class="fab fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fab fa-youtube"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <p>Copyrights © 2020 Klexed. All Rights Reserved.</p>
+            </div>
+        </footer>
+
+    </main>
+
+    <script src="main.js"></script>
+</body>
+
+</html>
